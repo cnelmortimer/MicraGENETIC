83fb5164814f20d7455f946f28c03ffd34196b77
cnelmortimer <cnelmortimer@users.noreply.github.com>
2015-04-18 23:07:42 +0200

    Update Lanzadera.cpp

--- Lanzadera.cpp
+++ Lanzadera.cpp
@@ -3,36 +3,36 @@
 #include "MicraGENETIC.h"
 
 class IndividuoSimple{//Individuo de ejemplo en: Obtener el maximo de la funcion y = x en [0, 100]
-	public:
-		IndividuoSimple(){
-		    this->valor = 0;
+    public:
+        IndividuoSimple(){
+            this->valor = 0;
         }
 
-		IndividuoSimple(int valor){
+        IndividuoSimple(int valor){
             this->valor = valor;
-		}
+        }
 
-		IndividuoSimple(const IndividuoSimple& referencia){//Operador de copia  (No requerido)
-			this->valor = referencia.valor;
-		}
+        IndividuoSimple(const IndividuoSimple& referencia){//Operador de copia  (No requerido)
+            this->valor = referencia.valor;
+        }
 
-		IndividuoSimple& operator = (const IndividuoSimple& referencia){//Operador de asignacion  (No requerido)
+        IndividuoSimple& operator = (const IndividuoSimple& referencia){//Operador de asignacion  (No requerido)
             if(this != &referencia){
                 this->valor = referencia.valor;
             }
             return *this;
-		}
+        }
 
-		IndividuoSimple reproducir(IndividuoSimple progenitorB){
-			IndividuoSimple descendiente((valor + progenitorB.valor)/2);//Valor medio como ejemplo (Sin tener en cuenta la forma de la funcion...)
-			return descendiente;
-		}
+        IndividuoSimple reproducir(IndividuoSimple progenitorB){
+            IndividuoSimple descendiente((valor + progenitorB.valor)/2);//Valor medio como ejemplo (Sin tener en cuenta la forma de la funcion...)
+            return descendiente;
+        }
 
-		void generarAleatoriamente(){
-			this->valor = rand() % 101;
-		}
+        void generarAleatoriamente(){
+            this->valor = rand() % 101;
+        }
 
-		void mutar(){//Se puede avanzar en +-5
+        void mutar(){//Se puede avanzar en +-5
             int preValor = rand() % 6;
             if(drand48()>0.5){
                 preValor = -preValor;
@@ -41,24 +41,24 @@ class IndividuoSimple{//Individuo de ejemplo en: Obtener el maximo de la funcion
             if(preValor>=0 && preValor<=100){//Si lamutacion hace al individuo inconsistente no se aplica
                 this->valor = preValor;
             }
-		}
+        }
 
-		bool operator > (const IndividuoSimple& str) const{
+        bool operator > (const IndividuoSimple& str) const{
             return (valor > str.valor);
         }
 
-		int obtenerAptitud(){
-			return valor;
-		}
+        int obtenerAptitud(){
+            return valor;
+        }
 
-		int leerVariable(){//Metodo adicional que no se requiere por la libreria aunque se añade por cuenta propia sin problemas
-			return valor;
-		}
+        int leerVariable(){//Metodo adicional que no se requiere por la libreria aunque se añade por cuenta propia sin problemas
+            return valor;
+        }
 
-		~IndividuoSimple(){}
+        ~IndividuoSimple(){}
 
-	private:
-		int valor;
+    private:
+        int valor;
 };
 
 int main(){//Uso:
