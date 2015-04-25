c4b8300501dc2a32be2183e45e74bbc24e342749
cnelmortimer <cnelmortimer@users.noreply.github.com>
2015-04-25 10:28:38 +0200

    Update Lanzadera.cpp

--- Lanzadera.cpp
+++ Lanzadera.cpp
@@ -5,11 +5,11 @@
 class IndividuoSimple{//Individuo de ejemplo en: Obtener el maximo de la funcion y = x en [0, 100]
 	public:
 		IndividuoSimple(){
-		    this->valor = 0;
-        }
+			this->valor = 0;
+		}
 
 		IndividuoSimple(int valor){
-            this->valor = valor;
+			this->valor = valor;
 		}
 
 		IndividuoSimple(const IndividuoSimple& referencia){//Operador de copia
@@ -26,19 +26,19 @@ class IndividuoSimple{//Individuo de ejemplo en: Obtener el maximo de la funcion
 		}
 
 		void mutar(){//Se puede avanzar en +-5
-            int preValor = rand() % 6;
-            if(drand48()>0.5){
-                preValor = -preValor;
-            }
-            preValor += valor;
-            if(preValor>=0 && preValor<=100){
-                this->valor = preValor;
-            }
+			int preValor = rand() % 6;
+			if(drand48()>0.5){
+				preValor = -preValor;
+			}
+			preValor += valor;
+			if(preValor>=0 && preValor<=100){
+				this->valor = preValor;
+			}
 		}
 
 		bool operator > (const IndividuoSimple& str) const{
-            return (valor > str.valor);
-        }
+			return (valor > str.valor);
+		}
 
 		int obtenerAptitud(){
 			return valor;
@@ -55,15 +55,15 @@ class IndividuoSimple{//Individuo de ejemplo en: Obtener el maximo de la funcion
 };
 
 int main(){
-    //50 individuos, 20 de ellos se reproducen por ciclo, probabilidad de solicitar mutacion del 0.5, seleccion por torneo
-    MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelTorneo);
-    ///Ejemplos adicionales:
-    //MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelTorneo, 6);//Torneos de 6 participantes por progenitor
-    //MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelAleatorioPuro);//Seleccion aleatoria de cada progenitor
-    //MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelRuleta);
-    //MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5);//Equivale a usar ruleta: Por defecto, seleccion de reproductores por ruleta
-    optimizador.reiniciarSemillasAleatorias();
-    IndividuoSimple resultado = optimizador.optimizarPorCiclos(10000);
-    printf("Mejor Resultado: %d\n", resultado.leerVariable());
+	//50 individuos, 20 de ellos se reproducen por ciclo, probabilidad de solicitar mutacion del 0.5, seleccion por torneo
+	MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelTorneo);
+	///Ejemplos adicionales:
+	//MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelTorneo, 6);//Torneos de 6 participantes por progenitor
+	//MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelAleatorioPuro);//Seleccion aleatoria de cada progenitor
+	//MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5, SelRuleta);
+	//MicraGENETIC<IndividuoSimple> optimizador(50, 20, 0.5);//Equivale a usar ruleta: Por defecto, seleccion de reproductores por ruleta
+	optimizador.reiniciarSemillasAleatorias();
+	IndividuoSimple resultado = optimizador.optimizarPorCiclos(10000);
+	printf("Mejor Resultado: %d\n", resultado.leerVariable());
 	return 0;
 }
