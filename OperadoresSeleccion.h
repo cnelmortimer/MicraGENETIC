336ccb6b8d96214bbb60f7f1a1d0b7f9b9300054
cnelmortimer <cnelmortimer@users.noreply.github.com>
2015-05-10 16:30:07 +0200

    Versión 3.0
    El cambio en los índices no mejora el rendimiento-> Versión 3.1 descartada
    Eliminación de parámetro innecesario en la selección por ruleta

--- OperadoresSeleccion.h
+++ OperadoresSeleccion.h
@@ -1,5 +1,5 @@
-//Operadores de seleccion alternativos (inyectado la reproduccion y mutacion para optimizar)
-//Archivo de expansion del nucleo MicraGENETIC (en su versio 3.0)
+//Complementos alternativos al operador de seleccion (inyectado la reproduccion y mutacion para optimizar)
+//Archivo de expansion del nucleo MicraGENETIC (versio 3.0)
 //Autor: Nicolas C
 
 #ifndef OPERADORESSELECCION_H_INCLUDED
@@ -7,7 +7,7 @@
 
 ///Seleccion por ruleta:
 template <class T> void seleccionRuleta(T* poblacion, double valorTotalPoblacion, int nIndividuos,
-                                           int nDescendientes, int tTorneo, double probabilidadMutacion,
+                                           int nReproductores, double probabilidadMutacion,
                                            T* descendencia){
     double q[nIndividuos], preQ = 0.0, vRuleta, qPuntual;//Seleccion por ruleta:
     for(int i = 0; i<nIndividuos; i++){
@@ -15,8 +15,8 @@ template <class T> void seleccionRuleta(T* poblacion, double valorTotalPoblacion
         q[i] = qPuntual;
         preQ = qPuntual;
     }//Hacer la seleccion de individuos sobre la poblacion:
-    int focoSeleccionA, focoSeleccionB;
-    for(int i = 0; i<nDescendientes; i++){//Desenrollado manual. Prescindiendo de una funcion de busqueda adicional
+    int focoSeleccionA, focoSeleccionB, nSeleccionados = 0;
+    for(int i = 0; i<nReproductores; i+=2){//Desenrollado manual. Prescindiendo de una funcion de busqueda adicional
         vRuleta = drand48();
         focoSeleccionA = 0; focoSeleccionB = 0;
         while(focoSeleccionA<nIndividuos){
@@ -36,28 +36,31 @@ template <class T> void seleccionRuleta(T* poblacion, double valorTotalPoblacion
         if(drand48()<probabilidadMutacion){//¿Solicitar mutacion?
             descendiente.mutar();
         }
-        descendencia[i] = descendiente;
+        descendencia[nSeleccionados] = descendiente;
+        nSeleccionados++;
     }
 }
 
 ///Seleccion aleatoria pura:
-template <class T> void seleccionAleatoriaPura(T* poblacion, int nIndividuos, int nDescendientes,
+template <class T> void seleccionAleatoriaPura(T* poblacion, int nIndividuos, int nReproductores,
                                                   double probabilidadMutacion, T* descendencia){
-    for(int i = 0; i<nDescendientes; i++){
+    int nSeleccionados = 0;
+    for(int i = 0; i<nReproductores; i+=2){
         T descendiente = poblacion[rand()%nIndividuos].reproducir(poblacion[rand()%nIndividuos]);
         if(drand48()<probabilidadMutacion){//¿Solicitar mutacion?
             descendiente.mutar();
         }
-        descendencia[i] = descendiente;
+        descendencia[nSeleccionados] = descendiente;
+        nSeleccionados++;
     }
 }
 
 ///Seleccion por torneos
-template <class T> void seleccionPorTorneo(T* poblacion, int nIndividuos, int nDescendientes,
+template <class T> void seleccionPorTorneo(T* poblacion, int nIndividuos, int nReproductores,
                                                 int tTorneo, double probabilidadMutacion, T* descendencia){
-    int progA = 0, progB = 0, j, candidato;
+    int nSeleccionados = 0, progA, progB, j, candidato;
     double aptA = -1, aptB = -1, aptCandidata;
-    for(int i = 0; i<nDescendientes; i++){
+    for(int i = 0; i<nReproductores; i+=2){
         for(j = 0; j<tTorneo; j++){//Inyectando los torneos para los dos progenitores
             candidato = rand()%nIndividuos;//Candidatos a progenitores tipo A
             aptCandidata = poblacion[candidato].obtenerAptitud();
@@ -74,7 +77,8 @@ template <class T> void seleccionPorTorneo(T* poblacion, int nIndividuos, int nD
         if(drand48()<probabilidadMutacion){//¿Solicitar mutacion?
             descendiente.mutar();
         }
-        descendencia[i] = descendiente;
+        descendencia[nSeleccionados] = descendiente;
+        nSeleccionados++;
     }
 }
 
