4fad9a763127df31053ebb3274bccb6ecd818aae
cnelmortimer <cnelmortimer@users.noreply.github.com>
2015-05-11 22:15:49 +0200

    Versión 3.1
    Optimización sencilla en los operadores de selección

--- MicraGENETIC.h
+++ MicraGENETIC.h
@@ -5,7 +5,7 @@
 #include <ctime>//Para iniciar la semilla con el tiempo
 #include <functional>//Para acceso a greater
 #include <algorithm>//Para SORT
-#include "OperadoresSeleccion.h"//Para metodos de seleccion alternativos al predeterminado (ruleta)
+#include "OperadoresSeleccion.h"//Distintas opciones al proceso de seleccion de reprocutores
 
 //Descripcion: 	Implementacion de un algoritmo genetico basico basado en plantillas. Se confia en que los objetos implementen IIndividuo.
 //Detalles: Seleccion de reproductores por ruleta (predeterminado)/torneo/aleatorio puro
@@ -18,7 +18,7 @@ template <class T> class MicraGENETIC{
 	public:
 		//Constructor-> Se requiere el numero de individuos total, cuantos de ellos son reproductores y el vector que los contiene
 		MicraGENETIC(int nIndividuos, int nReproductores, double probabilidadMutacion,
-			   TiposSeleccion opSelector=SelRuleta, int tTorneo=4);
+               TiposSeleccion opSelector=SelRuleta, int tTorneo=4);
 		T optimizarPorCiclos(int nCiclos);//El proceso de optimizacion no se detiene hasta completar el numero de ciclos dado
 		T optmizacionMixta(int nCiclosMaximos, double aptitudAceptable);//La optmizacion se realiza hasta consumir los ciclos o llegar a un valor de aptitud aceptable en la mejor solucion
 		//Nota: 		No se incluye un proceso basado solo en alcanzar una aptitud aceptable porque podria no terminar nunca
@@ -36,31 +36,31 @@ template <class T> class MicraGENETIC{
 		void setTTorneo(int tTorneo);
 		int getTTorneo();
 	private:
-		//Atributos configurables:
+	    //Atributos configurables:
 		int nReproductores;
 		int nIndividuos;
 		double probabilidadMutacion;
 		//Atributos gestionados solo internamente para ahorrar calculos:
-		int nDescendientes;
-		int nIndividuosYnDescendientes;
-		T* poblacion;//Region de memoria para guardar los individuos de cada ciclo
-		T* nuevaPoblacion;//Region de memoria para la poblacion del ciclo siguiente (mas consumo de memoria constante, menos reservas dinamicas)
-		double valorTotalPoblacion;//Para asignar las porciones de la seleccion por ruleta. Se calcula conforme se van alterando las poblaciones
+        int nDescendientes;
+        int nIndividuosYnDescendientes;
+        T* poblacion;//Region de memoria para guardar los individuos de cada ciclo
+        T* nuevaPoblacion;//Region de memoria para la poblacion del ciclo siguiente (mas consumo de memoria constante, menos reservas dinamicas)
+        double valorTotalPoblacion;//Para asignar las porciones de la seleccion por ruleta. Se calcula conforme se van alterando las poblaciones
 		T* descendencia;//Region de memoria para guardar los descendientes de cada ciclo
 		T* finDescendencia;
 		//Fnuciones internas:
 		void limpiarEntorno();
 		void generarPoblacionInicial();
 		void escogerReproducirMutar();
-		void seleccionarReemplazo();
-		TiposSeleccion opSelector;
-		int tTorneo;//Tamagno del torneo para este tipo de seleccion
+        void seleccionarReemplazo();
+        TiposSeleccion opSelector;
+        int tTorneo;//Tamagno del torneo para este tipo de seleccion
 };
 
 ///IMPLEMENTACION:
 ///Constructor del entorno de optimizacion
 template <class T> MicraGENETIC<T>::MicraGENETIC(int nIndividuos, int nReproductores, double probabilidadMutacion,
-												 TiposSeleccion opSelector, int tTorneo){
+                                                 TiposSeleccion opSelector, int tTorneo){
 	this->nIndividuos = nIndividuos;
 	this->nReproductores = nReproductores;
 	this->nDescendientes = nReproductores/2;
@@ -74,11 +74,11 @@ template <class T> MicraGENETIC<T>::MicraGENETIC(int nIndividuos, int nReproduct
 template <class T> T MicraGENETIC<T>::optimizarPorCiclos(int nCiclos){
 	generarPoblacionInicial();
 	for(int i = 0; i<nCiclos; i++){//"i" es el numero de ciclo
-		escogerReproducirMutar();
-		seleccionarReemplazo();
+        escogerReproducirMutar();
+        seleccionarReemplazo();
 	}
 	T resultado = poblacion[0];
-	limpiarEntorno();
+    limpiarEntorno();
 	return resultado;
 }
 
@@ -87,8 +87,8 @@ template <class T> T MicraGENETIC<T>::optmizacionMixta(int nCiclosMaximos, doubl
 	int ciclosConsumidos = 0;
 	generarPoblacionInicial();
 	while(ciclosConsumidos<nCiclosMaximos && poblacion[0].obtenerAptitud()<aptitudAceptable){
-		escogerReproducirMutar();
-		seleccionarReemplazo();
+        escogerReproducirMutar();
+        seleccionarReemplazo();
 		ciclosConsumidos++;
 	}
 	T resultado = poblacion[0];
@@ -97,8 +97,8 @@ template <class T> T MicraGENETIC<T>::optmizacionMixta(int nCiclosMaximos, doubl
 }
 
 template <class T> void MicraGENETIC<T>::reiniciarSemillasAleatorias(){
-	srand48(time(NULL));
-	srand(time(NULL));
+    srand48(time(NULL));
+    srand(time(NULL));
 }
 
 ///Funciones internas:
@@ -118,53 +118,53 @@ template <class T> void MicraGENETIC<T>::generarPoblacionInicial(){
 }
 
 template <class T> void MicraGENETIC<T>::escogerReproducirMutar(){
-	switch(opSelector){//Alternativa: Usar "Punteros a funciones"
-		case SelRuleta:
-			seleccionRuleta<T>(poblacion, valorTotalPoblacion, nIndividuos,
-										   nReproductores, probabilidadMutacion,
-										   descendencia); break;
-		case SelAleatorioPuro:
-			seleccionAleatoriaPura<T>(poblacion, nIndividuos, nReproductores, probabilidadMutacion,
-								   descendencia); break;
-		case SelTorneo:
-			seleccionPorTorneo<T>(poblacion, nIndividuos, nReproductores, tTorneo, probabilidadMutacion,
-							   descendencia);break;
-	}
-	sort(this->descendencia, this->finDescendencia, std::greater<T>());//Ordenar la descendencia
+    switch(opSelector){//Alternativa: Usar "Punteros a funciones"
+        case SelRuleta:
+            seleccionRuleta(poblacion, valorTotalPoblacion, nIndividuos, nDescendientes, probabilidadMutacion,
+                                           descendencia); break;
+        case SelAleatorioPuro:
+            seleccionAleatoriaPura(poblacion, nIndividuos, nDescendientes, probabilidadMutacion,
+                                   descendencia); break;
+        case SelTorneo:
+            seleccionPorTorneo(poblacion, nIndividuos, nDescendientes, tTorneo, probabilidadMutacion,
+                               descendencia);
+            break;
+    }
+    sort(this->descendencia, this->finDescendencia, std::greater<T>());//Ordenar la descendencia
 }
 
 template <class T> void MicraGENETIC<T>::seleccionarReemplazo(){//Tras los experimentos, resulta mucho mas rapido re-asignar nueva memoria que reubicar los elementos
-	int escogidos = 0, focoPoblacionPrevia = 0, focoDescendencia = 0;
-	this->valorTotalPoblacion = 0.0;//Reset para la nueva
-	while(escogidos<nIndividuos && focoDescendencia<nDescendientes){
-		if(poblacion[focoPoblacionPrevia].obtenerAptitud()>descendencia[focoDescendencia].obtenerAptitud()){
-			nuevaPoblacion[escogidos] = poblacion[focoPoblacionPrevia];
-			focoPoblacionPrevia++;
-		}else{
-			nuevaPoblacion[escogidos] = descendencia[focoDescendencia];
-			focoDescendencia++;
-		}
-		valorTotalPoblacion += nuevaPoblacion[escogidos].obtenerAptitud();
-		escogidos++;
-	}
-	if(escogidos<nIndividuos){//Si se llega aqui puede ser porque ya se han cogido todos los requeridos o porque ya solo importan los de la poblacion previa
-		while(escogidos<nIndividuos){
-			nuevaPoblacion[escogidos] = poblacion[focoPoblacionPrevia];
-			valorTotalPoblacion += poblacion[focoPoblacionPrevia].obtenerAptitud();
-			focoPoblacionPrevia++;
-			escogidos++;
-		}
-	}
-	T* swap = poblacion;//Ya no hay que destruir los elementos de la poblacion, se sobreescribiran como nueva poblacion...
-	this->poblacion = nuevaPoblacion;//Se ha construido de forma ordenada
-	this->nuevaPoblacion = swap;
+    int escogidos = 0, focoPoblacionPrevia = 0, focoDescendencia = 0;
+    this->valorTotalPoblacion = 0.0;//Reset para la nueva
+    while(escogidos<nIndividuos && focoDescendencia<nDescendientes){
+        if(poblacion[focoPoblacionPrevia].obtenerAptitud()>descendencia[focoDescendencia].obtenerAptitud()){
+            nuevaPoblacion[escogidos] = poblacion[focoPoblacionPrevia];
+            focoPoblacionPrevia++;
+        }else{
+            nuevaPoblacion[escogidos] = descendencia[focoDescendencia];
+            focoDescendencia++;
+        }
+        valorTotalPoblacion += nuevaPoblacion[escogidos].obtenerAptitud();
+        escogidos++;
+    }
+    if(escogidos<nIndividuos){//Si se llega aqui puede ser porque ya se han cogido todos los requeridos o porque ya solo importan los de la poblacion previa
+        while(escogidos<nIndividuos){
+            nuevaPoblacion[escogidos] = poblacion[focoPoblacionPrevia];
+            valorTotalPoblacion += poblacion[focoPoblacionPrevia].obtenerAptitud();
+            focoPoblacionPrevia++;
+            escogidos++;
+        }
+    }
+    T* swap = poblacion;//Ya no hay que destruir los elementos de la poblacion, se sobreescribiran como nueva poblacion...
+    this->poblacion = nuevaPoblacion;//Se ha construido de forma ordenada
+    this->nuevaPoblacion = swap;
 }
 
 ///Destructor:
 template <class T> MicraGENETIC<T>::~MicraGENETIC(){}
 
 template <class T> void MicraGENETIC<T>::limpiarEntorno(){
-	delete [] poblacion;
+    delete [] poblacion;
 	delete [] nuevaPoblacion;
 	delete [] descendencia;
 }
@@ -185,7 +185,7 @@ template <class T> int MicraGENETIC<T>::getNReproductores(){
 
 template <class T> void MicraGENETIC<T>::setNReproductores(int nReproductores){
 	this->nDescendientes = nReproductores/2;
-	this->nReproductores = nReproductores;
+    this->nReproductores = nReproductores;
 	this->nIndividuosYnDescendientes = nIndividuos +  nDescendientes;
 }
 
@@ -198,19 +198,19 @@ template <class T> void MicraGENETIC<T>::setProbabilidadMutacion(double probabil
 }
 
 template <class T> void MicraGENETIC<T>::setOpSelector(TiposSeleccion opSelector){
-	this->opSelector = opSelector;
+    this->opSelector = opSelector;
 }
 
 template <class T> TiposSeleccion MicraGENETIC<T>::getOpSelector(){
-	return opSelector;
+    return opSelector;
 }
 
 template <class T> void MicraGENETIC<T>::setTTorneo(int tTorneo){
-	this->tTorneo = tTorneo;
+    this->tTorneo = tTorneo;
 }
 
 template <class T> int MicraGENETIC<T>::getTTorneo(){
-	return tTorneo;
+    return tTorneo;
 }
 
 #endif
